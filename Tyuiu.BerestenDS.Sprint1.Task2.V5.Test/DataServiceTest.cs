c1a1d8120cbab55ae9ab060290db56c5c3ef41f7
Денис Берестень <d.blyl1337@gmail.com>
2024-09-23 22:26:00 +0500

    Task2

---
+++ Tyuiu.BerestenDS.Sprint1.Task2.V5.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+using Tyuiu.BerestenDS.Sprint1.Task2.V5.Lib;
+namespace Tyuiu.BerestenDS.Sprint1.Task2.V5.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int x = 2;
+            var res = ds.CalculateSideSquare(x);
+            Assert.AreEqual(16, res);
+        }
+    }
+}
