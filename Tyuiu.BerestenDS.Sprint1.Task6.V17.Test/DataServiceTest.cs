d946a3c3a40e1bddaa49e66658b172d5dc4d72ef
Денис Берестень <d.blyl1337@gmail.com>
2024-09-28 20:04:13 +0500

    Task 6

---
+++ Tyuiu.BerestenDS.Sprint1.Task6.V17.Test/DataServiceTest.cs
@@ -0,0 +1,13 @@
+using Tyuiu.BerestenDS.Sprint1.Task6.V17.Lib;
+namespace Tyuiu.BerestenDS.Sprint1.Task6.V17.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidString()
+        {
+            Assert.AreEqual(true, new DataService().CheckPalindrome("казак"));
+        }
+    }
+}
---
+++ Tyuiu.BerestenDS.Sprint1.Task6.V17/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.BerestenDS.Sprint1.Task6.V17.Lib;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Title = "Спринт #1 | Выполнил: Берестень Д. С.  | ИБКСБ-24-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Работа со строками класс String                                   *");
+        Console.WriteLine("* Задание #6                                                              *");
+        Console.WriteLine("* Вариант #17                                                             *");
+        Console.WriteLine("* Выполнил: Берестень Денис Сергеевич  | ИБКСБ 24-1                       *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу: пользователь вводит текст.                          *");
+        Console.WriteLine("* Проверить, что строка является перевертышем.                            *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Введите текст:");
+        string text = Console.ReadLine();
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        var result = new DataService().CheckPalindrome(text);
+        Console.WriteLine(result
+            ? "Строка является перевертышем"
+            : "Строка не является перевертышем");
+        Console.ReadKey();
+    }
+}
